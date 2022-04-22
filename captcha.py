3b54cd278e47bc1e29172ac91102ba73b453a6c3
Cassiano Flores <cassiano.flores2424@gmail.com>
2022-04-22 01:58:03 -0300

    finish v2

--- captcha.py
+++ captcha.py
@@ -1,9 +1,26 @@
+from copyreg import remove_extension
 import PIL.ImageGrab
 import pyautogui
 import time
 
 azul_fraco = (0, 39, 234)
 azul_forte = (71, 162, 257)
+laranja_fraco = (241, 146, 0)
+laranja_forte = (257, 190, 100)
+vermelho_fraco = (250, 5, 0)
+vermelho_forte = (254, 76, 74)
+roxo_fraco = (184, 0, 250)
+roxo_forte = (234, 102, 254)
+branco_fraco = (230, 230, 230)
+branco_forte = (252, 252, 252)
+rosa_fraco = (250, 94, 205)
+rosa_forte = (260, 183, 237)
+amarelo_fraco = (238, 226, 19)
+amarelo_forte = (257, 256, 122)
+verde_fraco = (0, 250, 8)
+verde_forte = (138, 254, 106)
+preto_fraco = (0, 0, 0)
+preto_forte = (30, 30, 30)
 
 
 teste = True;
@@ -21,28 +38,28 @@ while (teste == True):
     if captcha >= azul_fraco and captcha <= azul_forte:  # azul
         optionCaptcha = (124, 144, 169)
 
-    if captcha >= (241, 146, 0) and captcha <= (257, 190, 100):  # laranja
+    if captcha >= laranja_fraco and captcha <= laranja_forte:  # laranja
         optionCaptcha = (169, 134, 104)
 
-    if captcha >= (250, 5, 0) and captcha <= (254, 76, 74):  # vermelho
+    if captcha >= vermelho_fraco and captcha <= vermelho_forte:  # vermelho
         optionCaptcha = (144, 104, 104)
 
-    if captcha >= (184, 0, 250) and captcha <= (234, 102, 254):  # roxo
+    if captcha >= roxo_fraco and captcha <= roxo_forte:  # roxo
         optionCaptcha = (154, 113, 169)
 
-    if captcha >= (230, 230, 230) and captcha <= (252, 252, 252):  # branco
+    if captcha >= branco_fraco and captcha <= branco_forte:  # branco
         optionCaptcha = (184, 184, 184)
 
-    if captcha >= (250, 94, 205) and captcha <= (260, 183, 237):  # rosa
+    if captcha >= rosa_fraco and captcha <= rosa_forte:  # rosa
         optionCaptcha = (179, 134, 149)
 
-    if captcha >= (238, 226, 19) and captcha <= (257, 256, 122):  # amarelo
+    if captcha >= amarelo_fraco and captcha <= amarelo_forte:  # amarelo
         optionCaptcha = (174, 174, 104)
 
-    if captcha >= (0, 250, 8) and captcha <= (138, 254, 106):  # verde
+    if captcha >= verde_fraco and captcha <= verde_forte:  # verde
         optionCaptcha = (134, 164, 93)
 
-    if captcha >= (0, 0, 0) and captcha <= (30, 30, 30):  # preto
+    if captcha >= preto_fraco and captcha <= preto_forte:  # preto
         optionCaptcha = (93, 93, 93)
 
 
