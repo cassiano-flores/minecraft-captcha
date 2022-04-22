7902093176aaef649e74f18d03f80af1b4c2767c
Cassiano Flores <cassiano.flores2424@gmail.com>
2022-04-22 03:19:05 -0300

    finish v3

--- captcha.py
+++ captcha.py
@@ -34,31 +34,31 @@ while (teste == True):
 # o captcha e suas alternativas nao possuem as cores identicas
 # dependendo do rgb do captcha, armazena em optionCaptcha a cor que sera a resposta
 
-    if captcha.index(0) >= azul_fraco.index(0) and captcha <= azul_forte.index(0) and captcha.index(1) >= azul_fraco.index(1) and captcha <= azul_forte.index(1) and captcha.index(2) >= azul_fraco.index(2) and captcha <= azul_forte.index(2):
+    if captcha[0] >= azul_fraco[0] and captcha <= azul_forte[0] and captcha[1] >= azul_fraco[1] and captcha <= azul_forte[1] and captcha[2] >= azul_fraco[2] and captcha <= azul_forte[2]:
         optionCaptcha = (124, 144, 169) # azul
 
-    if captcha.index(0) >= laranja_fraco.index(0) and captcha <= laranja_forte.index(0) and captcha.index(1) >= laranja_fraco.index(1) and captcha <= laranja_forte.index(1) and captcha.index(2) >= laranja_fraco.index(2) and captcha <= laranja_forte.index(2):  
+    if captcha[0] >= laranja_fraco[0] and captcha <= laranja_forte[0] and captcha[1] >= laranja_fraco[1] and captcha <= laranja_forte[1] and captcha[2] >= laranja_fraco[2] and captcha <= laranja_forte[2]:  
         optionCaptcha = (169, 134, 104) # laranja
 
-    if captcha.index(0) >= vermelho_fraco.index(0) and captcha <= vermelho_forte.index(0) and captcha.index(1) >= vermelho_fraco.index(1) and captcha <= vermelho_forte.index(1) and captcha.index(2) >= vermelho_fraco.index(2) and captcha <= vermelho_forte.index(2): 
+    if captcha[0] >= vermelho_fraco[0] and captcha <= vermelho_forte[0] and captcha[1] >= vermelho_fraco[1] and captcha <= vermelho_forte[1] and captcha[2] >= vermelho_fraco[2] and captcha <= vermelho_forte[2]: 
         optionCaptcha = (144, 104, 104) # vermelho
 
-    if captcha.index(0) >= roxo_fraco.index(0) and captcha <= roxo_forte.index(0) and captcha.index(1) >= roxo_fraco.index(1) and captcha <= roxo_forte.index(1) and captcha.index(2) >= roxo_fraco.index(2) and captcha <= roxo_forte.index(2):  
+    if captcha[0] >= roxo_fraco[0] and captcha <= roxo_forte[0] and captcha[1] >= roxo_fraco[1] and captcha <= roxo_forte[1] and captcha[2] >= roxo_fraco[2] and captcha <= roxo_forte[2]:  
         optionCaptcha = (154, 113, 169) # roxo
 
-    if captcha.index(0) >= branco_fraco.index(0) and captcha <= branco_forte.index(0) and captcha.index(1) >= branco_fraco.index(1) and captcha <= branco_forte.index(1) and captcha.index(2) >= branco_fraco.index(2) and captcha <= branco_forte.index(2):  
+    if captcha[0] >= branco_fraco[0] and captcha <= branco_forte[0] and captcha[1] >= branco_fraco[1] and captcha <= branco_forte[1] and captcha[2] >= branco_fraco[2] and captcha <= branco_forte[2]:  
         optionCaptcha = (184, 184, 184) # branco
 
-    if captcha.index(0) >= rosa_fraco.index(0) and captcha <= rosa_forte.index(0) and captcha.index(1) >= rosa_fraco.index(1) and captcha <= rosa_forte.index(1) and captcha.index(2) >= rosa_fraco.index(2) and captcha <= rosa_forte.index(2):  
+    if captcha[0] >= rosa_fraco[0] and captcha <= rosa_forte[0] and captcha[1] >= rosa_fraco[1] and captcha <= rosa_forte[1] and captcha[2] >= rosa_fraco[2] and captcha <= rosa_forte[2]:  
         optionCaptcha = (179, 134, 149) # rosa
 
-    if captcha.index(0) >= amarelo_fraco.index(0) and captcha <= amarelo_forte.index(0) and captcha.index(1) >= amarelo_fraco.index(1) and captcha <= amarelo_forte.index(1) and captcha.index(2) >= amarelo_fraco.index(2) and captcha <= amarelo_forte.index(2):  
+    if captcha[0] >= amarelo_fraco[0] and captcha <= amarelo_forte[0] and captcha[1] >= amarelo_fraco[1] and captcha <= amarelo_forte[1] and captcha[2] >= amarelo_fraco[2] and captcha <= amarelo_forte[2]:  
         optionCaptcha = (174, 174, 104) # amarelo
 
-    if captcha.index(0) >= verde_fraco.index(0) and captcha <= verde_forte.index(0) and captcha.index(1) >= verde_fraco.index(1) and captcha <= verde_forte.index(1) and captcha.index(2) >= verde_fraco.index(2) and captcha <= verde_forte.index(2):  
+    if captcha[0] >= verde_fraco[0] and captcha <= verde_forte[0] and captcha[1] >= verde_fraco[1] and captcha <= verde_forte[1] and captcha[2] >= verde_fraco[2] and captcha <= verde_forte[2]:  
         optionCaptcha = (134, 164, 93) # verde
 
-    if captcha.index(0) >= preto_fraco.index(0) and captcha <= preto_forte.index(0) and captcha.index(1) >= preto_fraco.index(1) and captcha <= preto_forte.index(1) and captcha.index(2) >= preto_fraco.index(2) and captcha <= preto_forte.index(2):  
+    if captcha[0] >= preto_fraco[0] and captcha <= preto_forte[0] and captcha[1] >= preto_fraco[1] and captcha <= preto_forte[1] and captcha[2] >= preto_fraco[2] and captcha <= preto_forte[2]:  
         optionCaptcha = (93, 93, 93) # preto
 
 
